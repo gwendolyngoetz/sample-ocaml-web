c9285eda35014acb4d95b031e0a790b743bb9970
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-08-12 17:14:38 -0700

    Adding more samples, returning html fragments, simple htmx swapping.

--- webtools/bin/main.ml
+++ webtools/bin/main.ml
@@ -1,56 +1,106 @@
 open Tyxml.Html
 
 let make_link url text = 
-    a ~a:[a_href url; a_style "padding-right: 1rem;"]
+    a ~a:[a_href url;]
         [txt text]
 
 let header_nav =
-  ul ~a:[a_class ["links_bar"]; a_id "links_bar"]
-    [li ~a:[a_id "home_click"]
-       [txt "My Musings"];
-     li ~a:[a_id "about_click"]
-       [txt "About Me"];
-     li ~a:[a_id "blog_posts_click"]
-       [txt "Blog"];
-     li ~a:[a_id "hackathons_click"]
-       [txt "Hackathons"]]
+    header [
+      nav [
+        ul ~a:[a_class ["links_bar"]; a_id "links_bar"] [
+           li ~a:[a_id "home_click"]
+              [make_link "/" "Home"];
+           li ~a:[a_id "about_click"]
+              [make_link "/page-1" "Page-1"];
+           li ~a:[a_id "blog_posts_click"]
+              [make_link "/page-2" "Page-2"];
+        ]
+      ]
+    ]
 
 
-let layout pagetitle = 
+let layout_full pagetitle = 
     html
-        (head (title (txt pagetitle)) [])
+        (head (
+            title (txt pagetitle)) [
+            link ~rel:[`Icon] ~href:"/assets/img/favicon-32x32.png" ();
+            link ~rel:[`Stylesheet] ~href:"/assets/css/main.css" ();
+        ])
         (body [
             header_nav;
-            h1 [
-                txt "Hello!";
-            ];
-            h2 [txt "Header 2"];
-            a ~a:[
-                a_href "ocaml.org"; 
-                a_class ["class123"]; 
-                a_style "padding-right: 1rem;"
+            main [
+              h1 [txt pagetitle];
+              p [txt "Lipsum";];
             ]
-            [
-                txt "OCaml!"
-            ];
-            make_link "/" "Homei";
-            make_link "/page-1" "Page 1";
-            make_link "/page-2" "Page 2";
         ])
+
+let layout pagetitle page_content = 
+    html
+        (head (
+            title (txt pagetitle)) [
+            link ~rel:[`Icon] ~href:"/assets/img/favicon-32x32.png" ();
+            link ~rel:[`Stylesheet] ~href:"/assets/css/main.css" ();
+            script ~a:[a_src (Xml.uri_of_string "https://unpkg.com/htmx.org@1.9.4")] (txt "")
+        ])
+        (body [
+            header_nav;
+            main [
+              h1 [txt pagetitle];
+              p [txt "Lipsum";];
+              page_content;
+              div [
+                  button ~a:[
+                    Unsafe.string_attrib "hx-post" "/fragment-1";
+                    Unsafe.string_attrib "hx-trigger" "click";
+                    Unsafe.string_attrib "hx-swap" "innerHTML";
+                    Unsafe.string_attrib "hx-target" "#swap-target";
+                  ] 
+                  [txt "show fragment-1"];
+                  button ~a:[
+                    Unsafe.string_attrib "hx-post" "/fragment-2";
+                    Unsafe.string_attrib "hx-trigger" "click";
+                    Unsafe.string_attrib "hx-swap" "innerHTML";
+                    Unsafe.string_attrib "hx-target" "#swap-target";
+                  ] 
+                  [txt "show fragment-2"];
+              ];
+              div ~a:[a_id "swap-target"][]
+            ]
+        ])
+
+let html_fragment fragment_text =
+    div ~a:[a_class ["div123"]] [ 
+        h1 [ txt fragment_text ] 
+    ]
+
+let page2_content = make_link "zzz" "ixxx"
 
 let html_to_string html = 
     Format.asprintf "%a" (Tyxml.Html.pp ()) html
 
+let html_fragment_to_string html = 
+    Format.asprintf "%a" (Tyxml.Html.pp_elt ()) html
+
 let () =
   Dream.run
   @@ Dream.logger
-  @@ Dream_livereload.inject_script()
   @@ Dream.router [
     Dream.get "/" (fun _ ->
-      Dream.html (html_to_string(layout "Home")));
+      Dream.html (html_to_string(layout_full "Home")));
+
     Dream.get "/page-1" (fun _ ->
-      Dream.html (html_to_string(layout "Page 1")));
+      Dream.html (html_to_string(layout_full "Page 1")));
+
     Dream.get "/page-2" (fun _ ->
-      Dream.html (html_to_string(layout "Page 2")));
-    Dream_livereload.route()
+      Dream.html (html_to_string(layout "Page 2" page2_content)));
+
+    Dream.post "/fragment-1" (fun _ ->
+      Dream.html (html_fragment_to_string(html_fragment "fragment 1")));
+
+    Dream.post "/fragment-2" (fun _ ->
+      Dream.html (html_fragment_to_string(html_fragment "fragment 2")));
+
+    Dream.get "/assets/**" @@ Dream.static "assets";
+
+    Dream_livereload.route();
   ]
